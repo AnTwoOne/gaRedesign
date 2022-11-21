2084d80f4037116d9abf7332484cd269f8683f54
AnTwoOne <antoin84260@gmail.com>
2022-11-21 19:12:29 +0100

    bla

--- js/tabClass.js
+++ js/tabClass.js
@@ -1,93 +1,116 @@
 //We want to create a class for building tabs
 
 export default class gaTabs {
-	constructor(selector, options = {}, addOns = {}) {
-		this.selector = selector;
-		this.options = options;
-		this.addOns = addOns;
-	}
+  constructor(selector, options = {}, addOns = {}) {
+    this.selector = selector;
+    this.options = options;
+    this.addOns = addOns;
+  }
 
-	init() {
-		const tab = document.querySelector(`${this.selector}`);
-		if (tab !== undefined && tab !== null) {
-			const tabContents = tab.querySelectorAll('.tab-content');
-			const tabOpeners = tab.querySelectorAll('.tab-opener');
-			let tabToggles = null;
-			//Adding the height to the tabs element
-			if (this.options.height) {
-				tab.style.height = `${this.options.height}`;
-			}
+  init() {
+    const tab = document.querySelector(`${this.selector}`);
+    if (tab !== undefined && tab !== null) {
+      const tabContents = tab.querySelectorAll(".tab-content");
+      const tabOpeners = tab.querySelectorAll(".tab-opener");
+      let tabToggles = null;
+      //Adding the height to the tabs element
+      if (this.options.height) {
+        tab.style.height = `${this.options.height}`;
+      }
 
-			if (this.options.tabToggle === true) {
-				tabToggles = tab.querySelectorAll('.tab-opener__toggle');
+      if (this.options.tabToggle === true) {
+        tabToggles = tab.querySelectorAll(".tab-opener__toggle");
+      }
+      const tabsBehavior = (tab) => {
+        const openTab = (tabOpener) => {
+		// Get the targeted tab content
+          const tabContent = tab.querySelector(
+            `.tab-content[data-openedBy="${tabOpener.dataset.opens}"]`
+          );
+		  // Get the other tab contents
+          const otherTabContents = tab.querySelectorAll(
+            `.tab-content:not([data-openedBy="${tabOpener.dataset.opens}"])`
+          );
+		  // Get the other tab openers
+          const otherTabOpeners = tab.querySelectorAll(
+            `.tab-opener:not([data-opens="${tabOpener.dataset.opens}"])`
+          );
+		  // If the tab has toggle option
+            if (this.options.tabToggle === true) {
+              const tabToggle = tab.querySelector(
+                `.tab-opener__toggle[data-opens="${tabOpener.dataset.opens}"]`
+              );
+              const otherTabToggles = tab.querySelectorAll(
+                `.tab-opener__toggle:not([data-opens="${tabOpener.dataset.opens}"])`
+              );
+              otherTabToggles.forEach((otherTabToggle) => {
+                otherTabToggle.classList.remove("js-tab-toggle-active");
+              });
+              tabToggle.classList.add("js-tab-toggle-active");
+            }
+          // If the tab has an option of animation
+          if (this.options.animation.status == true) {
+			console.log(this.options.animation.status, 'animation status');
+			//Get the current active tab
+			const currentActiveTab = tab.querySelector(".js-tab-content-active");
+			//If the current active tab is not the same as the tab that is being opened
+			if (currentActiveTab !== tabContent) {
+				console.log(currentActiveTab, 'tab to exit');
+				console.log(tabContent, 'tab to enter');
+				//Play the exit animation on the current active tab
+				this.options.animation.function(currentActiveTab, 'exit', this.options.animation.duration);
+				//Play the enter animation on the tab that is being opened
+				setTimeout(() => {
+					this.options.animation.function(tabContent, 'enter', this.options.animation.duration);
+				}, this.options.animation.duration - this.options.animation.duration * 0.4);
+				
+
+				otherTabOpeners.forEach((tabOpener) =>
+              		tabOpener.classList.remove("js-tab-opener-active")
+            	);
+				tabOpener.classList.add("js-tab-opener-active");
 			}
-			if (this.options.closeWhenOpen === true) {
-				const tabsBehavior = (tab) => {
-					const openTab = (tabOpener) => {
-						const tabContent = tab.querySelector(`.tab-content[data-openedBy="${tabOpener.dataset.opens}"]`);
-						const otherTabContents = tab.querySelectorAll(`.tab-content:not([data-openedBy="${tabOpener.dataset.opens}"])`);
-						const otherTabOpeners = tab.querySelectorAll(`.tab-opener:not([data-opens="${tabOpener.dataset.opens}"])`);
-						if (this.options.tabToggle === true) {
-							const tabToggle = tab.querySelector(`.tab-opener__toggle[data-opens="${tabOpener.dataset.opens}"]`);
-							const otherTabToggles = tab.querySelectorAll(`.tab-opener__toggle:not([data-opens="${tabOpener.dataset.opens}"])`);
-							otherTabToggles.forEach((otherTabToggle) => {
-								otherTabToggle.classList.remove('js-tab-toggle-active');
-							});
-							tabToggle.classList.add('js-tab-toggle-active');
-						}
-						otherTabContents.forEach((tabContent) => tabContent.classList.remove('js-tab-content-active'));
-						otherTabOpeners.forEach((tabOpener) => tabOpener.classList.remove('js-tab-opener-active'));
-						tabContent.classList.add('js-tab-content-active');
-						tabOpener.classList.add('js-tab-opener-active');
-					};
-					tabOpeners.forEach((tabOpener) => {
-						tabOpener.addEventListener('click', () => openTab(tabOpener));
-					});
-				};
-				if (this.addOns !== null && this.addOns !== undefined) {
-					Object.keys(this.addOns).forEach((addOn) => {
-						this.addOns[addOn](tab);
-					});
-				}
-				tabsBehavior(tab);
-			} else {
-				const tabsBehavior = (tab) => {
-					const openTab = (tabOpener) => {
-						const tabContent = tab.querySelector(`.tab-content[data-tab="${tabOpener.dataset.opens}"]`);
-						const otherTabContents = tab.querySelectorAll(`.tab-content:not([data-tab="${tabOpener.dataset.opens}"])`);
-						const otherTabOpeners = tab.querySelectorAll(`.tab-opener:not([data-opens="${tabOpener.dataset.opens}"])`);
-						if (this.options.tabToggle === true) {
-							const tabToggle = tab.querySelector(`.tab-opener__toggle[data-opens="${tabOpener.dataset.opens}"]`);
-							const otherTabToggles = tab.querySelectorAll(`.tab-opener__toggle:not([data-opens="${tabOpener.dataset.opens}"])`);
-							otherTabToggles.forEach((otherTabToggle) => {
-								otherTabToggle.classList.remove('js-tab-toggle-active');
-							});
-							tabToggle.classList.add('js-tab-toggle-active');
-						}
-						otherTabContents.forEach((tabContent) => tabContent.classList.remove('js-tab-content-active'));
-						otherTabOpeners.forEach((tabOpener) => tabOpener.classList.remove('js-tab-opener-active'));
-						tabContent.classList.add('js-tab-content-active');
-						tabOpener.classList.add('js-tab-opener-active');
-					};
-					tabOpeners.forEach((tabOpener) => {
-						tabOpener.addEventListener('click', () => openTab(tabOpener));
-					});
-					if (this.addOns !== null && this.addOns !== undefined) {
-						Object.keys(this.addOns).forEach((addOn) => {
-							this.addOns[addOn]();
-						});
-					}
-				};
-				tabsBehavior();
+			//If the current active tab is the same as the tab that is being opened
+			// Do nothing
 			}
+		// If the tab does not have an option of animation
+		else if (this.options.animation.status == false) {
+			// Remove the active class from the other tab openers
+			otherTabOpeners.forEach((tabOpener) =>
+			  tabOpener.classList.remove("js-tab-opener-active")
+			);
+			// Add the active class to the tab opener
+			tabOpener.classList.add("js-tab-opener-active");
+			// Remove the active class from the other tab contents
+			otherTabContents.forEach((tabContent) =>
+			  tabContent.classList.remove("js-tab-content-active")
+			);
+			// Add the active class to the tab content
+			tabContent.classList.add("js-tab-content-active");
+		  }
+		
+        };
+		// End of openTab function
+		// Add event listener to the tab openers and call the openTab function on click
+        tabOpeners.forEach((tabOpener) => {
+          tabOpener.addEventListener("click", () => openTab(tabOpener));
+        });
+	  };
+	  //End of tabsBehavior function
+	  //If the tab has any addOns then call the addOns functions
+	  if (this.addOns !== null && this.addOns !== undefined) {
+		// console.log(this.addOns);
+		Object.keys(this.addOns).forEach((addOn) => {
+			console.log('running an addOn');
+			this.addOns[addOn](tab);
+		});
 		}
-	}
-
-	get start() {
-		return this.init();
-	}
-
-	get extension() {
-		return this.addOns();
+	tabsBehavior(tab);
 	}
 }
+// End of the init function
+}
+
+
+
+
--- js/tabs.js
+++ js/tabs.js
@@ -83,12 +83,82 @@ const dividerBehavior = (tab) => {
 	});
 };
 
+// We want to create a function that will animated the tab content children element when the class js-tab-content-active is added to the tab content element
+// We  will create two animations, one for when the class is added and one for when the class is removed
+// We will use the animation library GSAP to create the animations
+
+const animateTabContent = (tabContent, direction, duration) => {
+
+	const tabContentOverlay = tabContent.querySelector(".tab-content__overlay");
+	const tabContentProductShot = tabContent.querySelector(".product-shot");
+
+	
+
+    // Create the enter animation with GSAP timeline
+    // The tab content background overlay will move right to left by 200px and fade in
+    // The tab content product shot will move from top to bottom by 50px and fade in
+	const enterAnimation = gsap.timeline();
+    
+    
+
+    // Create the exit animation with GSAP timeline
+    // The tab content background overlay will move left to right by 200px and fade out
+    // The tab content product shot will move from bottom to top by 50px and fade out
+    const exitAnimation = gsap.timeline();
+    
+    
+
+	if(direction === 'exit') {
+		// const exitAnimation = gsap.timeline();
+		exitAnimation.fromTo(tabContentOverlay, { x: 0, opacity: 1 }, { x: 200, opacity: 0, duration: (duration / 1000) - 0.2 });
+    	exitAnimation.fromTo(tabContentProductShot, { y: 0, opacity: 1 }, { y: -50, opacity: 0, duration: (duration / 1000) - 0.2 } , `-=${(duration / 1000) - 0.2}`);
+		//On complete we will remove the class js-tab-content-active from the tab content element
+		exitAnimation.eventCallback('onComplete', () => {
+			console.log('exit animation complete', tabContent);
+			tabContent.classList.remove('js-tab-content-active');
+		});
+		
+	} else {
+		tabContent.classList.add('js-tab-content-active') 
+		enterAnimation.fromTo(tabContentOverlay, { x: 200, opacity: 0 }, { x: 0, opacity: 1, duration: duration / 1000 });
+    	enterAnimation.fromTo(tabContentProductShot, { y: -50, opacity: 0 }, { y: 0, opacity: 1, duration: duration / 1000 }, `-=${duration / 1000}`);
+		//On complete we will add the js-tab-content-active class to the tab content
+	}
+}
+
+// Feature tab content animation
+const animateFeatureTabContent = (tabContent, direction, duration) => {
+
+	const featureTabColumns = tabContent.querySelectorAll('.feature-tab__col');
+
+	const enterAnimation = gsap.timeline();
+	const exitAnimation = gsap.timeline();
+
+	if(direction === 'exit') {
+		exitAnimation.fromTo(tabContent, { opacity: 1 }, { opacity: 0, duration: (duration / 1000) });
+		exitAnimation.fromTo(featureTabColumns[0], { y: 0}, { y: -15, duration: (duration / 1000) - 0.2 }, `-=${(duration / 1000) }`);
+		exitAnimation.fromTo(featureTabColumns[1], { y: -46}, { y: 0, duration: (duration / 1000) - 0.2 }, `-=${(duration / 1000)}`);
+		
+		exitAnimation.eventCallback('onComplete', () => {
+			console.log('exit animation complete', tabContent);
+			tabContent.classList.remove('js-tab-content-active');
+		});
+		
+	} else {
+		tabContent.classList.add('js-tab-content-active');
+		enterAnimation.fromTo(tabContent, { opacity: 0 }, { opacity: 1, duration: duration / 1000 });
+		enterAnimation.fromTo(featureTabColumns[0], { y: 15}, { y: 0, duration: duration / 1000 - 0.2 }, `-=${duration / 1000} - 0.2`);
+		enterAnimation.fromTo(featureTabColumns[1], { y: 0}, { y: -46, duration: duration / 1000 - 0.2 }, `-=${duration / 1000} - 0.2`);
+		
+	}
+}
+
 const tab = new gaTabs(
 	'.tabs',
-	{ tabToggle: false, closeWhenOpen: true },
+	{ tabToggle: false, animation: {status: true, function: animateTabContent, duration: 3000} },
 	{
 		dividerBehavior: dividerBehavior
 	}
 ).init();
 
-const featureTab = new gaTabs('.features-tab-container', { tabToggle: false, closeWhenOpen: true }).init();
+const featureTab = new gaTabs('.features-tab-container', { tabToggle: false, animation: {status: true, function: animateFeatureTabContent, duration: 500 } }).init();
